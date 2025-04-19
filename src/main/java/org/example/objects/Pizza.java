b12688d45c95546a22258f87a62d9566758b0abd
Mikhail Bakulev <michael.baculev@gmail.com>
2025-04-19 14:01:51 +0400

    add: добавлен log4j2

--- src/main/java/org/example/objects/Pizza.java
+++ src/main/java/org/example/objects/Pizza.java
@@ -1,9 +1,13 @@
 package org.example.objects;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.example.exceptions.PizzaSizeException;
 
 // Общий класс для всех пицц
 public abstract class Pizza extends Dish {
+    Logger logger = LogManager.getLogger(Pizza.class);
+
     protected int size;
     protected Pizza(String name, int size, double price) {
         super(name, price);
@@ -11,8 +15,7 @@ public abstract class Pizza extends Dish {
             if (size <=0 || size >= 10) throw new PizzaSizeException(size);
             this.size = size;
         } catch (PizzaSizeException exception) {
-            // TODO Заменить на logger
-            System.out.println("Ошибка размера пиццы");
+            logger.error("Ошибка размера пиццы");
         }
     }
 }
